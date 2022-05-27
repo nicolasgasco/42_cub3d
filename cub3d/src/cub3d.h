09fbc227ec4a8a24555cfd362cfbbe01b1143daa
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-05-27 22:36:09 +0200

    Open scene file for reading and validation

--- cub3d/src/cub3d.h
+++ cub3d/src/cub3d.h
@@ -17,10 +17,13 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /* Scene description file validation */
 void    ft_check_num_args(int argc);
 void    ft_scene_desc_file_validation(char *file_path);
 void    ft_file_extension_validation(char *file_path);
+void    ft_type_ids_validation(char *file_path);
+int    ft_open_scene_file(char *file_path);
 
 #endif
