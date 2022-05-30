3dc776c6e22f2e2085353ce00c8ec9f2dbbeef53
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-05-30 22:19:30 +0200

    Fix issue with scene file having extension but no filename

--- cub3d/src/scene_desc_file_validation/ft_scene_desc_file_validation-file_ext.c
+++ cub3d/src/scene_desc_file_validation/ft_scene_desc_file_validation-file_ext.c
@@ -9,6 +9,11 @@ void ft_file_extension_validation(char *file_path)
 
 	EXT_LEN = 4;
 	ext_start = ft_strlen(file_path) - EXT_LEN;
+	if (ext_start == 0)
+	{
+		ft_putendl_fd("Error: invalid extension", STDERR_FILENO);
+		exit(2);
+	}
 	file_path_ext = ft_substr(file_path, ext_start, EXT_LEN);
 	if (ft_strncmp(file_path_ext, ".cub", EXT_LEN) != 0)
 	{
