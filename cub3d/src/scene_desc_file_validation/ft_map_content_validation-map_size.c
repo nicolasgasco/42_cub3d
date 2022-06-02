c35062fa81242c0b3a2adca2f70c88cb0562df4c
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2022-06-02 19:31:55 +0200

    Refactor error message not outputting to stderr (#43)

--- cub3d/src/scene_desc_file_validation/ft_map_content_validation-map_size.c
+++ cub3d/src/scene_desc_file_validation/ft_map_content_validation-map_size.c
@@ -1,15 +1,16 @@
 #include "../cub3d.h"
+#include "../../Libft/libft.h"
 
 void	ft_validate_size(t_map *map)
 {
     if (map->height < 3)
     {
-        printf("Error. Incorrect map size: height must be over 3 rows\n");
+        ft_putendl_fd("Error. Incorrect map size: height must be over 3 rows", STDERR_FILENO);
 	exit(11);
     }
     if (map->width < 3)
     {
-	printf("Error. Incorrect map size: width must be over 3 columns\n");
+        ft_putendl_fd("Error. Incorrect map size: width must be over 3 columns", STDERR_FILENO);
 	exit(12);
     }
 }
