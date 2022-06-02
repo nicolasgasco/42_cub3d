545a4b9dae9e6700f242158fdfcc9a10a88a3e83
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-02 20:21:35 +0200

    Fix memory leaks with map width validation

--- cub3d/src/scene_desc_file_validation/ft_map_content_validation-map_size.c
+++ cub3d/src/scene_desc_file_validation/ft_map_content_validation-map_size.c
@@ -12,6 +12,8 @@ void	ft_validate_size(t_map *map)
     }
     if (map->width < 3)
     {
+        ft_free_allocated_map_data(map);
+        ft_free_map_struct(map);
         ft_putendl_fd("Error. Incorrect map size: width must be over 3 columns", STDERR_FILENO);
 	exit(12);
     }
