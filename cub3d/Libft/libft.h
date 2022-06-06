3d1cba7fe2ba08e2a307eef3d8eb359eac33c51f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-06 15:21:40 +0200

    Add ft_putstr to Libft

--- cub3d/Libft/libft.h
+++ cub3d/Libft/libft.h
@@ -18,15 +18,16 @@
 
 size_t	ft_strlen(const char *s);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
-int	    ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-int     ft_atoi(const char *str);
-int     ft_isdigit(int c);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int c);
 char	*ft_strdup(const char *s1);
 void	*ft_memset(void *l, int c, size_t len);
 
