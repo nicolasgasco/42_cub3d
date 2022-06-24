c8980912deb1d4f45088dc37275c6e1efd4bde5f
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-06-24 13:20:07 +0200

    Refactor logic to scale textures

--- cub3d/src/render_window/ft_render_scene-downscale.c
+++ cub3d/src/render_window/ft_render_scene-downscale.c
@@ -18,11 +18,10 @@ void	ft_render_downscaled_texture(t_map *map)
 	int	divider;
 
 	divider = TEXTURE_SIZE / (TEXTURE_SIZE - map->slc->height) + 1;
-	ft_render_downscaled_first_half(map, divider);
-	ft_render_downscaled_second_half(map, divider);
+	ft_downscaled_texture_loop(map, divider);
 }
 
-void	ft_render_downscaled_first_half(t_map *map, int divider)
+void	ft_downscaled_texture_loop(t_map *map, int divider)
 {
 	int	x;
 	int	i;
@@ -32,14 +31,22 @@ void	ft_render_downscaled_first_half(t_map *map, int divider)
 	i = 0;
 	limit = map->slc->height / 4;
 	while (i < limit && map->y < PROJ_PLANE_HEIGHT) // Second condition TBD
-		ft_render_downscaled_first_half_pixel_put(map, divider, &i, &x);
-	x = (TEXTURE_SIZE / 4);
+		ft_downscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 4) - 1;
 	limit = map->slc->height / 2;
 	while (i < limit && map->y < PROJ_PLANE_HEIGHT) // Second condition TBD
-		ft_render_downscaled_first_half_pixel_put(map, divider, &i, &x);
+		ft_downscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 2) - 1;
+	limit = map->slc->height / 4 * 3;
+	while (i < limit && map->y < PROJ_PLANE_HEIGHT) // Second condition TBD
+		ft_downscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 4) * 3 - 1;
+	limit = map->slc->height;
+	while (i < limit && map->y < PROJ_PLANE_HEIGHT) // Second condition TBD
+		ft_downscaled_texture_loop_pixel_put(map, divider, &i, &x);
 }
 
-void	ft_render_downscaled_first_half_pixel_put(t_map *map, int divider,
+void	ft_downscaled_texture_loop_pixel_put(t_map *map, int divider,
 	int *i, int *x)
 {
 	if (*x % divider != 0)
@@ -50,34 +57,4 @@ void	ft_render_downscaled_first_half_pixel_put(t_map *map, int divider,
 		map->y += 1;
 	}
 	*x += 1;
-}
-
-void	ft_render_downscaled_second_half(t_map *map, int divider)
-{
-	int	i;
-	int	x;
-	int	limit;
-
-	i = map->slc->height - 1;
-	x = TEXTURE_SIZE - 1;
-	limit = map->slc->height / 4 * 3;
-	while (i > limit)
-		ft_render_downscaled_second_half_pixel_put(map, divider, &i, &x);
-	x = TEXTURE_SIZE - (TEXTURE_SIZE / 4);
-	limit = map->slc->height / 2;
-	while (i >= limit)
-		ft_render_downscaled_second_half_pixel_put(map, divider, &i, &x);
-}
-
-void	ft_render_downscaled_second_half_pixel_put(t_map *map, int divider,
-	int *i, int *x)
-{
-	if (*x % divider != 0)
-	{
-		my_mlx_pixel_put(map->view->plane_data, map->slc->column, map->y,
-			map->rdata->textures[NO_TEXTURE_INDEX].texture_columns[*x][0]);
-		*i -= 1;
-		map->y += 1;
-	}
-	*x -= 1;
 }
--- cub3d/src/render_window/ft_render_scene-upscale.c
+++ cub3d/src/render_window/ft_render_scene-upscale.c
@@ -21,21 +21,18 @@ void	ft_render_upscaled_texture(t_map *map)
 	{
 		divider = (map->slc->height / TEXTURE_SIZE) + 1;
 		printf("Multiplier\n");
-		ft_render_multiply_first_half(map, divider);
-		ft_render_multiply_second_half(map, divider);
-		// ft_render_solid_color(map);
+		ft_multiplied_texture_loop(map, divider);
 	}
 	else
 	{
 		divider = TEXTURE_SIZE / (map->slc->height - TEXTURE_SIZE);
 		if (divider == 1)
 			divider++;
-		ft_render_upscaled_first_half(map, divider);
-		ft_render_upscaled_second_half(map, divider);
+		ft_upscaled_texture_loop(map, divider);
 	}
 }
 
-void	ft_render_upscaled_first_half(t_map *map, int divider)
+void	ft_upscaled_texture_loop(t_map *map, int divider)
 {
 	int	x;
 	int	i;
@@ -45,31 +42,22 @@ void	ft_render_upscaled_first_half(t_map *map, int divider)
 	i = 0;
 	limit = map->slc->height / 4;
 	while (i < limit)
-		ft_render_upscaled_first_half_pixel_put(map, divider, &i, &x);
-	x = (TEXTURE_SIZE / 4);
+		ft_upscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 4) - 1;
 	limit = map->slc->height / 2;
 	while (i < limit)
-		ft_render_upscaled_first_half_pixel_put(map, divider, &i, &x);
-}
-
-void	ft_render_upscaled_second_half(t_map *map, int divider)
-{
-	int	i;
-	int	x;
-	int	limit;
-
-	i = map->slc->height - 1;
-	x = TEXTURE_SIZE - 1;
+		ft_upscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 2) - 1;
 	limit = map->slc->height / 4 * 3;
-	while (i > limit)
-		ft_render_upscaled_second_half_pixel_put(map, divider, &i, &x);
-	x = TEXTURE_SIZE - (TEXTURE_SIZE / 4);
-	limit = map->slc->height / 2;
-	while (i > limit)
-		ft_render_upscaled_second_half_pixel_put(map, divider, &i, &x);
+	while (i < limit)
+		ft_upscaled_texture_loop_pixel_put(map, divider, &i, &x);
+	x = (TEXTURE_SIZE / 4) * 3 - 1;
+	limit = map->slc->height;
+	while (i < limit)
+		ft_upscaled_texture_loop_pixel_put(map, divider, &i, &x);
 }
 
-void	ft_render_upscaled_first_half_pixel_put(t_map *map, int divider,
+void	ft_upscaled_texture_loop_pixel_put(t_map *map, int divider,
 	int *i, int *x)
 {
 	my_mlx_pixel_put(map->view->plane_data, map->slc->column, map->y,
@@ -78,15 +66,4 @@ void	ft_render_upscaled_first_half_pixel_put(t_map *map, int divider,
 		*x += 1;
 	map->y += 1;
 	*i += 1;
-}
-
-void	ft_render_upscaled_second_half_pixel_put(t_map *map, int divider,
-	int *i, int *x)
-{
-	my_mlx_pixel_put(map->view->plane_data, map->slc->column, map->y,
-		map->rdata->textures[NO_TEXTURE_INDEX].texture_columns[*x][0]);
-	if ((*i + 1) % divider != 0)
-		*x -= 1;
-	*i -= 1;
-	map->y += 1;
 }
