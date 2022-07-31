cd4e67b84f6edd3bd7fe7f9a1dfc56c1e59f0fb6
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2022-07-31 14:12:56 +0200

    Refactor post validation data manip logic for clarity (#100)

--- cub3d/src/post_validation_data_manip/ft_map_data_manip.c
+++ cub3d/src/post_validation_data_manip/ft_map_data_manip.c
@@ -13,19 +13,24 @@
 #include "../cub3d.h"
 #include "../../Libft/libft.h"
 
-void ft_post_validation_data_manip(t_map *map, t_rdata *rdata)
+void ft_post_validation_data_manip(t_map *map)
 {
-	rdata->c_col_int = ft_rgb_str_to_int(map->c_color);
-	ft_write_debug_msg_int("Ceiling color is ", rdata->c_col_int);
-	rdata->f_col_int = ft_rgb_str_to_int(map->f_color);
-	ft_write_debug_msg_int("Floor color is ", rdata->f_col_int);
-	ft_validate_texture_files(map, rdata);
-	rdata->textures = (t_tdata *)malloc(sizeof(t_tdata) * NUM_TEXTURES);
-	rdata->textures[NO_TEXTURE_INDEX] = ft_parse_texture_file(map->no_path);
-	rdata->textures[EA_TEXTURE_INDEX] = ft_parse_texture_file(map->ea_path);
-	rdata->textures[SO_TEXTURE_INDEX] = ft_parse_texture_file(map->so_path);
-	rdata->textures[WE_TEXTURE_INDEX] = ft_parse_texture_file(map->we_path);
-	ft_write_debug_msg_int("All textures parsed ", 0);
+	map->rdata->c_col_int = ft_rgb_str_to_int(map->c_color);
+	ft_write_debug_msg_int("Ceiling color is ", map->rdata->c_col_int);
+	map->rdata->f_col_int = ft_rgb_str_to_int(map->f_color);
+	ft_write_debug_msg_int("Floor color is ", map->rdata->f_col_int);
+	ft_validate_texture_files(map);
+	ft_parse_all_texture_files(map);
+}
+
+void	ft_parse_all_texture_files(t_map *map)
+{
+	map->rdata->textures = (t_tdata *)malloc(sizeof(t_tdata) * NUM_TEXTURES);
+	map->rdata->textures[NO_TEXTURE_INDEX] = ft_parse_texture_file(map->no_path);
+	map->rdata->textures[EA_TEXTURE_INDEX] = ft_parse_texture_file(map->ea_path);
+	map->rdata->textures[SO_TEXTURE_INDEX] = ft_parse_texture_file(map->so_path);
+	map->rdata->textures[WE_TEXTURE_INDEX] = ft_parse_texture_file(map->we_path);
+	ft_write_debug_msg_int("All textures parsed ", 0);	
 }
 
 t_tdata ft_parse_texture_file(char *texture_path)
