09e4e34df01319a601ec08afb6241d0817776b2a
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-08-05 23:50:20 +0200

    Add computed vertical movement

--- cub3d/src/render_window/ft_scene_movements.c
+++ cub3d/src/render_window/ft_scene_movements.c
@@ -13,51 +13,55 @@
 #include "../cub3d.h"
 #include "../../Libft/libft.h"
 
-void    ft_move_forward(t_map *map)
+void	ft_move_forward(t_map *map)
 {
-    int is_facing_down;
+	double	radians;
+	int		v_increase;
+	int		l_increase;
 
-    is_facing_down = ft_is_facing_down(ft_transform_angle(map->prj->view_angle));
-    if (is_facing_down)
-        map->prj->player->y += V_MOVEMENT_INCREMENT;
-    else
-        map->prj->player->y -= V_MOVEMENT_INCREMENT;
+	radians = map->prj->view_angle * (M_PI / 180);
+	v_increase = V_MOVEMENT_INCREMENT * sin(radians);
+	l_increase = V_MOVEMENT_INCREMENT * cos(radians);
+	map->prj->player->y -= v_increase;
+	map->prj->player->x += l_increase;
 }
 
-void    ft_move_back(t_map *map)
+void	ft_move_back(t_map *map)
 {
-   int is_facing_down;
+	double	radians;
+	int		v_increase;
+	int		l_increase;
 
-    is_facing_down = ft_is_facing_down(ft_transform_angle(map->prj->view_angle));
-    if (is_facing_down)
-        map->prj->player->y -= V_MOVEMENT_INCREMENT;
-    else
-        map->prj->player->y += V_MOVEMENT_INCREMENT;
+	radians = map->prj->view_angle * (M_PI / 180);
+	v_increase = V_MOVEMENT_INCREMENT * sin(radians);
+	l_increase = V_MOVEMENT_INCREMENT * cos(radians);
+	map->prj->player->y += v_increase;
+	map->prj->player->x -= l_increase;
 }
 
-void    ft_rotate_leftwards(t_map *map)
+void	ft_rotate_leftwards(t_map *map)
 {
-    int  new_angle;
+	int	new_angle;
 
-    new_angle = (map->prj->view_angle + L_MOVEMENT_INCREMENT) % 360;
-    map->prj->view_angle = new_angle;
+	new_angle = (map->prj->view_angle + L_MOVEMENT_INCREMENT) % 360;
+	map->prj->view_angle = new_angle;
 }
 
-void    ft_rotate_rightwards(t_map *map)
+void	ft_rotate_rightwards(t_map *map)
 {
-    int new_angle;
-    int current_angle;
+	int	new_angle;
+	int	current_angle;
 
-    current_angle = map->prj->view_angle;
-    if (current_angle == 0)
-        current_angle = 360;
-    else if (current_angle < 0)
-        current_angle = 360 - abs(current_angle);
-    new_angle = current_angle - L_MOVEMENT_INCREMENT;
-    map->prj->view_angle = new_angle;
+	current_angle = map->prj->view_angle;
+	if (current_angle == 0)
+		current_angle = 360;
+	else if (current_angle < 0)
+		current_angle = 360 - abs(current_angle);
+	new_angle = current_angle - L_MOVEMENT_INCREMENT;
+	map->prj->view_angle = new_angle;
 }
 
-int     ft_angle_to_degrees(int angle)
+int	ft_angle_to_degrees(int angle)
 {
-    return ((angle * FIELD_OF_VIEW) / WIN_WIDTH);
+	return ((angle * FIELD_OF_VIEW) / WIN_WIDTH);
 }
