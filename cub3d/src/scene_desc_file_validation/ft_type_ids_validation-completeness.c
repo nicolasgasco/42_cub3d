548df9f5330921434270241d417c623139f08a5b
Nicolas Gasco <nicolas.gasco92@gmail.com>
2022-08-07 23:12:24 +0200

    Pass Norminette

--- cub3d/src/scene_desc_file_validation/ft_type_ids_validation-completeness.c
+++ cub3d/src/scene_desc_file_validation/ft_type_ids_validation-completeness.c
@@ -15,20 +15,10 @@
 
 void	ft_type_ids_completeness_check(t_map *map)
 {
-	ft_completeness_check_colors(map);
-	ft_completeness_check_o_paths(map);
-}
-
-void	ft_completeness_check_colors(t_map *map)
-{
 	if (!map->c_color || !map->f_color)
 		ft_incomplete_scene_info_error_exit(map);
 	else if (!ft_strncmp(map->c_color, map->f_color, ft_strlen(map->c_color)))
 		ft_duplicate_scene_info_error_exit(map);
-}
-
-void	ft_completeness_check_o_paths(t_map *map)
-{
 	if (!map->no_path || !map->ea_path || !map->so_path || !map->we_path)
 		ft_incomplete_scene_info_error_exit(map);
 	ft_check_o_paths_duplicates(map);
