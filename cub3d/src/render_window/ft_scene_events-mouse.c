9ca0f615a8cd5f531a8d5e9ebeb7355df7c43e0e
Nicolas Gasco <73175085+nicolasgasco@users.noreply.github.com>
2022-08-07 17:11:17 +0200

    Add backwards movement with mouse (#115)

--- cub3d/src/render_window/ft_scene_events-mouse.c
+++ cub3d/src/render_window/ft_scene_events-mouse.c
@@ -13,9 +13,47 @@
 #include "../cub3d.h"
 #include "../../Libft/libft.h"
 
-void	ft_view_events(t_map *map)
+int	ft_mouse_events(int button, int x, int y, t_map *map)
 {
-	mlx_hook(map->view->mlx_win, ON_KEY_DOWN, 1L << 0, ft_keyboard_events, map);
-	mlx_mouse_hook(map->view->mlx_win, ft_mouse_events, map);
-	mlx_hook(map->view->mlx_win, ON_DESTROY, 0, ft_close_window, map);
+	if (button != LEFT_CLICK && y)
+		return (1);
+	ft_mouse_rotation(x, map);
+	ft_mouse_v_movement(y, map);
+	ft_render_game_scene(map);
+	return (0);
+}
+
+void	ft_mouse_rotation(int x, t_map *map)
+{
+	int	win_half;
+	int	degrees;
+
+	win_half = WIN_WIDTH / 2;
+	if (x >= win_half)
+	{
+		x -= win_half;
+		degrees = x * (FIELD_OF_VIEW / 2) / win_half;
+		if (degrees > L_MOVEMENT_INCREMENT)
+			degrees = L_MOVEMENT_INCREMENT;
+		ft_rotate_rightwards(map, degrees);
+	}
+	else
+	{
+		x = win_half - x;
+		degrees = x * (FIELD_OF_VIEW / 2) / win_half;
+		if (degrees > L_MOVEMENT_INCREMENT)
+			degrees = L_MOVEMENT_INCREMENT;
+		ft_rotate_leftwards(map, degrees);
+	}
+}
+
+void	ft_mouse_v_movement(int y, t_map *map)
+{
+	int	win_half;
+
+	win_half = WIN_WIDTH / 2;
+	if (y <= win_half)
+		ft_move_forward(map, V_MOVEMENT_INCREMENT);
+	else
+		ft_move_back(map, V_MOVEMENT_INCREMENT);
 }
